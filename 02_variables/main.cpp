831e49116e826776bc85846189034cdfcfa38041
Moustapha <s.moustapha.sy.ndoye@gmail.com>
2022-11-14 12:04:34 +0000

    feat: add variables datatypes examples

--- 02_variables/main.cpp
+++ 02_variables/main.cpp
@@ -3,12 +3,29 @@
 using namespace  std;
 
 int main() {
+    // 1. Varibale declaration
     // Use dataType variableName [= value]; to declare a new variable
     int developerAge;
     developerAge = 44;
     string developerName = "John";
     cout << "Hello, World! My name is " << developerName << endl;
     cout << "I am " << developerAge << " years old." << endl;
-    cout << "A year later..." << endl << "And now I am " << ++developerAge;
+    cout << "A year later..." << endl << "And now I am " << ++developerAge << endl;
+
+    // 2. Datatypes
+    char initial = 'M'; // Use chart to declare a single character string. The string have to be in single quotes.
+    string quote = "Stay hungry, Stay foolish";
+    int age = 55;
+    float pi = 3.14;
+    double  temperature = 68.7898;
+    bool isCompleted = true;
+
+    cout << "Initial : " << initial << endl;
+    cout << "Quote : " << quote << endl;
+    cout << "Age : " << age << endl;
+    cout << "Pi : " << pi << endl;
+    cout << "Temperature : " << temperature << endl;
+    cout << "Is completed ? : " << isCompleted << endl;
+
     return 0;
 }
